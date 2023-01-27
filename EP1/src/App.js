1ba933a2ebce5c5b57f410b1b448944d9a971263
Joshua Lehman <joshlehman.dev@gmail.com>
2023-01-27 05:58:01 -0500

    commit

--- EP1/src/App.js
+++ EP1/src/App.js
@@ -1,54 +1,42 @@
-import classes from "./App.module.scss";
-import NewSection from "./components/NewSection";
-import { useRef } from "react";
-
-import image1 from "./images/pexels-frans-van-heerden-624015.jpg";
-import image2 from "./images/pexels-luis-del-río-15286.jpg";
-import image3 from "./images/pexels-mengliu-di-3064079.jpg";
-import image4 from "./images/pexels-stanislav-kondratiev-2909077.jpg";
+import logo from "./logo.svg";
+import "./App.css";
+import ImageSlider from "./ImageSlider";
 
 function App() {
-  const section1 = useRef();
-  const section2 = useRef();
-  const section3 = useRef();
-  const section4 = useRef();
+  const slides = [
+    {
+      url: "http://localhost:3000/image-1.jpg",
+      title: "Beach",
+    },
+    {
+      url: "http://localhost:3000/image-2.jpg",
+      title: "Boat",
+    },
+    {
+      url: "http://localhost:3000/image-3.jpg",
+      title: "Forest",
+    },
+    {
+      url: "http://localhost:3000/image-4.jpg",
+      title: "City",
+    },
+    {
+      url: "http://localhost:3000/image-5.jpg",
+      title: "Italy",
+    },
+  ];
 
-  function scrollTo(section) {
-    section.current.scrollIntoView({ behavior: "smooth" });
-  }
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
 
   return (
-    <div className={classes.container}>
-      <div ref={section1}>
-        <NewSection
-          image={image1}
-          headline={"lorem ispum dolor"}
-          scrollTo={scrollTo}
-          goToSectionRef={section2}
-        />
-      </div>
-      <div ref={section2}>
-        <NewSection
-          image={image2}
-          headline={"lorem ispum dolor"}
-          scrollTo={scrollTo}
-          goToSectionRef={section3}
-        />
-      </div>
-      <div ref={section3}>
-        <NewSection
-          image={image3}
-          headline={"lorem ispum dolor"}
-          scrollTo={scrollTo}
-          goToSectionRef={section4}
-        />
-      </div>
-      <div ref={section4}>
-        <NewSection
-          image={image4}
-          headline={"lorem ispum dolor"}
-          scrollTo={scrollTo}
-        />
+    <div className="App">
+      <h1>Hello World</h1>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
       </div>
     </div>
   );
